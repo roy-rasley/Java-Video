3290a0d3f4338aa704cb719dc5354d1ce13f84bd
roy-rasley <rasley14@gmail.com>
2022-04-19 14:24:36 -0400

    round up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-       //implicit casting
-        //byte > short > int > long > float > double
-        String x = "1.1";
-        double y = Double.parseDouble(x) + 2;
-        System.out.println(y);
+       int result = (int)Math.ceil(1.1F);
+        System.out.println(result);
     }
 }
