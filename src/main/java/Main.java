4310e8584a085f352c0642aa58c52ac4cf9d6865
roy-rasley <rasley14@gmail.com>
2022-04-19 15:05:56 -0400

    currency formatting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
 import java.awt.*;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-       int result = (int)Math.max(50,100);
+      //formatting numbers
+       NumberFormat currency = NumberFormat.getCurrencyInstance();
+       String result = currency.format(1234567.891);
         System.out.println(result);
     }
 }
