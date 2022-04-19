c419218b579fc39087e71e6aec75f3643f9ef43f
roy-rasley <rasley14@gmail.com>
2022-04-19 15:14:30 -0400

    formatPercentage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,8 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
       //formatting numbers
-       NumberFormat currency = NumberFormat.getCurrencyInstance();
-       String result = currency.format(1234567.891);
+
+       String result = NumberFormat.getPercentInstance().format(0.1);
         System.out.println(result);
     }
 }
