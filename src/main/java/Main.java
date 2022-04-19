8bb9b1ddd0ccb2fcda2da506bb1ac0ea3a7640d7
roy-rasley <rasley14@gmail.com>
2022-04-19 16:11:20 -0400

    MCbasicinput

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Please enter your age:");
-      Scanner scanner = new Scanner(System.in);
-      byte age = scanner.nextByte();
-        System.out.println("You are "+ age);
+        //Gets need input from the user
+        System.out.print("Principal:");
+        Scanner scanner = new Scanner(System.in);
+        int principal = scanner.nextInt();
+        System.out.print("Annual interest rate: ");
+        float interestRate = scanner.nextFloat();
+        System.out.print("Period (Years): ");
+        int years = scanner.nextInt();
+
     }
 }
