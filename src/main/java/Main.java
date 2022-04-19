fc225f581f6b5297ebca0d9e03f7e23a45bd1392
roy-rasley <rasley14@gmail.com>
2022-04-19 13:25:26 -0400

    Arithmetic expression

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
+       int x = 1;
+        x++;
+        System.out.println(x);
 
     }
 }
