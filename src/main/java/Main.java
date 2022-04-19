5a69bcc9a465bae217d4ed976226115a28291ba8
roy-rasley <rasley14@gmail.com>
2022-04-19 13:49:49 -0400

    parse string to int

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
        //implicit casting
         //byte > short > int > long > float > double
-        double x = 1.1;
-        int y = (int)x + 2;
+        String x = "1";
+        int y = Integer.parseInt(x) + 2;
         System.out.println(y);
     }
 }
