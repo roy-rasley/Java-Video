9447bf625f1b5c861e4f5cdf2eb8f4a6be9fc258
roy-rasley <rasley14@gmail.com>
2022-04-19 13:31:16 -0400

    Order of operations fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-       int x = 10 + 3 * 2;
+       int x = 10 + (3 * 2);
         System.out.println(x);
 
 
