45a8ed5e02b9ad7f61a112abb04754d69534452f
roy-rasley <rasley14@gmail.com>
2022-04-20 12:20:28 -0400

    fizzbuzz code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,33 +4,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //Gets need input from the user
-        System.out.print("Principal:");
         Scanner scanner = new Scanner(System.in);
-        int principal = scanner.nextInt();
-        System.out.print("Annual interest rate: ");
-        float interestRate = scanner.nextFloat();
-        System.out.print("Period (Years): ");
-        int years = scanner.nextInt();
+        System.out.print("Number:");
+        int number = scanner.nextInt();
 
-        //calculations
-        int p = principal;
-        //yearly rate
-        double i = interestRate;
-        //monthly rate
-        double m = interestRate/100/12;
-        //number of payments
-        int n = (years*12);
-
-        //actual monthly payment calculation
-        double mortgagePayment = principal * (m * Math.pow(1 + m, n))
-                / (Math.pow(1 + m, n)-1);
-
-        //p(m(1+m)^n)/((1+m)^n-1);
-
-
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgagePayment);
-        System.out.println("Your monthly mortgage payment is "+mortgageFormatted);
-
+        //if statement for fizz, and buzz
+        if (number%5 == 0 && number%3 ==0){
+            System.out.println("fizzbuzz");
+        } else if (number%5 == 0) {
+            System.out.println("fizz");
+        }else if (number%3 == 0){
+            System.out.println("buzz");
+        }else{
+            System.out.println(number);
+        }
 
     }
 }
