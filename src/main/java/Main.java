8658c6e05e5521253e489bb97b70f983fc688796
roy-rasley <rasley14@gmail.com>
2022-04-21 15:00:07 -0400

    wordandlettercount

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,55 +1,51 @@
 import java.text.NumberFormat;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
-        int principal = 0;
-        float interestRate = 0;
-        int years = 0;
-        Scanner scanner = new Scanner(System.in);
-        //Gets need input from the user
-        while(true){
-        System.out.print("Principal ($1K-$1M):");
-        principal = scanner.nextInt();
-        if(principal>=1000 && principal<=1000000)
-            break;
-        System.out.println("Enter a value within the given parameters");
+   // count words in string, count number of "The" in sentence, count number of letters in last word.
+    Scanner input = new Scanner(System.in);
+    String string = input.nextLine();
+
+    StringTokenizer tokens = new StringTokenizer(string);
+    //counts number of words in string
+    int wordCount = tokens.countTokens();
+
+    int theCounter = 0;
+
+    int charInLastWord = 0;
+
+    //counts number of times "the" shows up in string
+    while(tokens.hasMoreTokens()){
+        if(tokens.nextToken().equalsIgnoreCase("the"))
+            theCounter++;
+    }
+
+    //count number of characters in last word of string
+        var st = new StringTokenizer(string);
+        var l = 0;
+        while(st.hasMoreTokens()){ // this goes through the tokenizer until there are no more tokens past the one it is on and will then print out the length of said token
+            l = st.nextToken().length();
+        }
+        System.out.println("The total number of words in the sentence is: "+wordCount);
+        System.out.println("The total number of times THE occurs is "+ theCounter + " times.");
+        if (string.endsWith(".")){
+            l--;
+        }
+        System.out.println("The last word contains " + l + " letters");
+
+
+
+
+
+        //System.out.println("The total number of characters in the last word is "+charInLastWord);
+
+
+
+
         }
 
-        while(true){
-            System.out.print("Annual interest rate: ");
-            interestRate = scanner.nextFloat();
-            if(interestRate>0&&interestRate<=30)
-                break;
-            System.out.println("Enter an interest rate within the given parameters");
-        }
-
-        while(true){
-            System.out.print("Period (Years): ");
-            years = scanner.nextInt();
-            if(years>0&&years<=30)
-                break;
-            System.out.println("Enter a value greater than one year and less than 30 years");
-        }
-
-        //calculations
-        int p = principal;
-        //yearly rate
-        double i = interestRate;
-        //monthly rate
-        double m = interestRate/100/12;
-        //number of payments
-        int n = (years*12);
-
-        //actual monthly payment calculation
-        double mortgagePayment = principal * (m * Math.pow(1 + m, n))
-                / (Math.pow(1 + m, n)-1);
-
-        //p(m(1+m)^n)/((1+m)^n-1);
-
-
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgagePayment);
-        System.out.println("Your monthly mortgage payment is "+mortgageFormatted);
 
     }
-}
+
